823e123f5d90094398eaccf1aaa1e5a2d3c1bbda
bruce <brucec@hotmail.com>
2021-09-30 10:52:18 -0700

    define Jequivalent

--- transferFunctionsDef.m
+++ transferFunctionsDef.m
@@ -1,4 +1,4 @@
-%% define constants
+%% define constants given in lab manual
 Kt = 0.72;
 Kb = 0.4173;
 Kvp = 111.55;
@@ -8,13 +8,19 @@ Ra = 6.5;
 La = 0.0375;
 Jm = 0.765*10^(-4);
 mt = 20;
-hp = 0.02;
-Ls = 820;
-ds = 20;
-ps = 7800;
+hp = 0.02;%m
+Ls = 0.82;%m
+ds = 0.02;%m
+ps = 7800;%kg/m^3
 Jenc = 1.7*10^(-4);
 Jcoup = 4*10^(-5);
 Jtach = 9.3212*10^(-7);
+
+%% define derived constants
+rs = ds/2;
+Jtable = mt*(hp/(2*pi))^2;
+JLeadScrew = 1/2*(pi*rs^2*Ls*ps)*rs^2;
+Jeq = Jtable + JLeadScrew + Jm + Jenc + Jcoup + Jtach;
 
 %% define transfer functions
 s = tf("s");
