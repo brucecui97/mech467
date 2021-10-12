9578d0da240fddd3aa908a860f1bdb6789c255b6
bruce <brucec@hotmail.com>
2021-10-11 18:49:27 -0700

    add torques up to 50

--- lab1/partA_analysis.m
+++ lab1/partA_analysis.m
@@ -26,9 +26,37 @@ forwardTorque25 = mean(motorTorque_Nm25(3111:18870));
 backwardTorque25 = mean(motorTorque_Nm25(21920:37780));
 
 mmPerSec30 = load("30.mat").output;
+voltage_v30 = mmPerSec30.CH1sig;
+motorCurrent_A30 = voltage_v30 * currentSensorGain;
+motorTorque_Nm30 = Kt * motorCurrent_A30;
+plot(motorTorque_Nm30);
+forwardTorque30 = mean(motorTorque_Nm30(3111:18870));
+backwardTorque30 = mean(motorTorque_Nm30(21920:37780));
+
 mmPerSec35 = load("35.mat").output;
+voltage_v35 = mmPerSec35.CH1sig;
+motorCurrent_A35 = voltage_v35 * currentSensorGain;
+motorTorque_Nm35 = Kt * motorCurrent_A35;
+plot(motorTorque_Nm35);
+forwardTorque35 = mean(motorTorque_Nm35(3111:18870));
+backwardTorque35 = mean(motorTorque_Nm35(21920:37780));
+
 mmPerSec40 = load("40.mat").output;
+voltage_v40 = mmPerSec40.CH1sig;
+motorCurrent_A40 = voltage_v40 * currentSensorGain;
+motorTorque_Nm40 = Kt * motorCurrent_A40;
+plot(motorTorque_Nm40);
+forwardTorque40 = mean(motorTorque_Nm40(1504:9988));
+backwardTorque40 = mean(motorTorque_Nm40(11530:20190));
+
 mmPerSec50 = load("50.mat").output;
+voltage_v50 = mmPerSec50.CH1sig;
+motorCurrent_A50 = voltage_v50 * currentSensorGain;
+motorTorque_Nm50 = Kt * motorCurrent_A50;
+plot(motorTorque_Nm50);
+forwardTorque50 = mean(motorTorque_Nm50(1504:9988));
+backwardTorque50 = mean(motorTorque_Nm50(11530:20190));
+
 mmPerSec60 = load("60.mat").output;
 mmPerSec80 = load("80.mat").output;
 mmPerSec100 = load("100.mat").output;
@@ -43,9 +71,10 @@ voltage_v = mmPerSec15.CH1sig;
 motorCurrent_A = voltage_v * currentSensorGain;
 motorTorque_Nm = Kt * motorCurrent_A;
 
-linear_speeds_mmPerSec=[15,20,25];
+linear_speeds_mmPerSec=[15,20,25,30,35,40,50];
 angular_speeds_radPerSec = linear_speeds_mmPerSec/1000/hp*2*pi;
-forward_torques = [forwardTorque15,forwardTorque20, forwardTorque25];
+forward_torques = [forwardTorque15,forwardTorque20, forwardTorque25, forwardTorque30, forwardTorque35, forwardTorque40, forwardTorque50];
+backward_torques = [backwardTorque15,backwardTorque20, backwardTorque25, backwardTorque30, backwardTorque35, backwardTorque40, backwardTorque50];
 plot(angular_speeds_radPerSec, forward_torques);
 
-test_B = (forward_torques(end)-forward_torques(1))/(angular_speeds_radPerSec(end)-angular_speeds_radPerSec(1))
+test_B = (forward_torques(end)-forward_torques(1))/(angular_speeds_radPerSec(end)-angular_speeds_radPerSec(1));
---
+++ lab1/simplified_tf.m
@@ -0,0 +1,68 @@
+%% define constants given in lab manual
+Kt = 0.72;
+Kb = 0.4173;
+Kvp = 111.55;
+Kvi = 3.0019*10^5;
+Sg = 0.887;
+Ra = 6.5;
+La = 0.0375;
+Jm = 0.765*10^(-4);
+mt = 20;
+hp = 0.02;%m
+Ls = 0.82;%m
+ds = 0.02;%m
+ps = 7800;%kg/m^3
+Jenc = 1.7*10^(-4);
+Jcoup = 4*10^(-5);
+Jtach = 9.3212*10^(-7);
+
+%% define derived constants
+rs = ds/2;
+Jtable = mt*(hp/(2*pi))^2;
+JLeadScrew = 1/2*(pi*rs^2*Ls*ps)*rs^2;
+Jeq = Jtable + JLeadScrew + Jm + Jenc + Jcoup + Jtach;
+B = 0.006; %Nm/rad/s temp value but will be found later in lab
+
+%% define transfer functions
+s = tf("s");
+currentSensorGain = Sg;
+pWMAmplifier = Kvp + Kvi/s;
+pWMAmplifier_simplified = Kvp + Kvi/s;
+backEmfConstant = Kb;
+electricalWindingOfMotor = 1/(La*s+Ra);
+electricalWindingOfMotor_simplified = 1/Ra
+motorConstant = Kt;
+mechanicalBlock = 1/(Jeq*s+B);
+
+% currentSensorGain = 1;
+% pWMAmplifier = 2;
+% backEmfConstant = 3;
+% electricalWindingOfMotor =4;
+% motorConstant = 5;
+% mechanicalBlock = 7;
+
+%% Td
+Td2Ws = -mechanicalBlock/(1+(-mechanicalBlock)*((1/(motorConstant*mechanicalBlock) + (backEmfConstant/pWMAmplifier))*motorConstant*electricalWindingOfMotor*(-pWMAmplifier)));
+
+%% plot transfer functions for Im2Ws
+Im2Ws = motorConstant * mechanicalBlock;
+step(Im2Ws);
+margin(Im2Ws);
+
+%% plot transfer functions for Iin2Im
+Iin2Im = minreal(pWMAmplifier*electricalWindingOfMotor/(1+pWMAmplifier*electricalWindingOfMotor+electricalWindingOfMotor*motorConstant*mechanicalBlock*backEmfConstant));
+step(Iin2Im);
+margin(Iin2Im);
+bandwidth(Iin2Im)
+dcgain(Iin2Im)
+
+%% plot transfer function for Vin to Ws
+Vin2Ws = currentSensorGain * Iin2Im * Im2Ws;
+step(Vin2Ws);
+margin(Vin2Ws);
+bandwidth(Vin2Ws)
+[y,tout] = step(Vin2Ws/s/dcgain(Vin2Ws));%ramp response
+plot(linspace(0,50,1000),linspace(0,50,1000));
+hold on
+plot(tout,y)
+dcgain(Vin2Ws)
