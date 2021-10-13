44c6df2205e199e4315529bcec4c0e594a056eda
bruce <brucec@hotmail.com>
2021-10-12 21:05:24 -0700

    add code to plot the W vs time

--- lab1/partCAnalysis.m
+++ lab1/partCAnalysis.m
@@ -1,5 +1,15 @@
 
 h1 = load("1.mat").output;
+
+time = h1.time;
+displacement_m = h1.CH1out/1000;
+angular_position_theta = displacement_m/hp*2*pi;
+Ts = 0.0005; %s
+W_t = deriv(angular_position_theta, Ts);
+W_dot_t = deriv(W_t,Ts);
+
+plot(time,W_t)
+
 h2= load("2.mat").output;
 h3= load("3.mat").output;
 h4= load("4.mat").output;
@@ -9,6 +19,7 @@ h7= load("7.mat").output;
 h8= load("8.mat").output;
 h9= load("9.mat").output;
 h10= load("10.mat").output;
+
 h20= load("20.mat").output;
 h30= load("30.mat").output;
 h40= load("40.mat").output;
