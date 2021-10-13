8f01c5eec842a2de9084dd3a78d13298e70d79e0
bruce <brucec@hotmail.com>
2021-10-12 21:01:34 -0700

    add part C anaylsis load data

---
+++ lab1/partCAnalysis.m
@@ -0,0 +1,25 @@
+
+h1 = load("1.mat").output;
+h2= load("2.mat").output;
+h3= load("3.mat").output;
+h4= load("4.mat").output;
+h5= load("5.mat").output;
+h6= load("6.mat").output;
+h7= load("7.mat").output;
+h8= load("8.mat").output;
+h9= load("9.mat").output;
+h10= load("10.mat").output;
+h20= load("20.mat").output;
+h30= load("30.mat").output;
+h40= load("40.mat").output;
+h50= load("50.mat").output;
+h60= load("60.mat").output;
+h70= load("70.mat").output;
+
+V_t = 10*sin(5*t)
+
+laplace(V_t)/s
+
+a= laplace(V_t)
+
+a/s
