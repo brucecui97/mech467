cf8ee57fcf2feb630bdcefa0bef04abb4ed4a24b
bruce <brucec@hotmail.com>
2021-11-10 17:17:25 -0800

    found error in discretizing plant formula

--- lab2/prelab.m
+++ lab2/prelab.m
@@ -14,15 +14,21 @@ b = [2904.286];
 a = [1, 8.743,0,0];
 [r,p,k] = residue(b,a);
 
-r(1)/(s-p(1)) + r(2)/(s-p(2)) + r(3)/(s-p(3))^2
-discretePlant = c2d(Vin2Xa_continous,T);
+partial_fraction = r(1)/(s-p(1)) + r(2)/(s-p(2)) + r(3)/(s-p(3))^2
+
+discretePlant = c2d(2904.286/(s^3+8.743*s^2),T);
 
 syms z
-temp = z/(z-1)*(332.1895*0.0002*z/(z-1)^2-37.9955*z/(z-1)+37.9955*z/(z-0.9983))
-[N,D] = numden(simplify(temp))
+temp = 38*z/(z-exp(-8.743*T))-38*z/(z-1) + 332.2*T*z/(z-1)^2
+z = 2;
+eval(temp*(z-1)/z)
 
-[N2,D2] = numden(simplify(D/z^2))
+%%
 
-z=2
-eval(temp)
+testTfContinoius = 1/(s)
+
+discretePlant = c2d(testTfContinoius,T)
+
+syms z
+z/(z-1)*z/(z-exp(-T))
 
