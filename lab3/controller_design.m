4f08991b2d52650bf44da5334528c409fa8a89fc
bruce <brucec@hotmail.com>
2021-11-25 15:33:49 -0800

    add code to find pole and zero

--- lab3/controller_design.m
+++ lab3/controller_design.m
@@ -9,12 +9,15 @@ Kty = 0.49;
 Key = 1.59;
 Jey = 3*10^(-4)
 By = 0.0091;
+Ts = 0.0001;
 
 s = tf("s");
 plantX = Kax*Ktx/(Jex*s+Bx)*(1/s)*Kex;
+discretePlantX = c2d(plantX,Ts,'zoh');
 plantY = Kay*Kty/(Jey*s+By)*(1/s)*Key;
+discretePlantY = c2d(plantY,Ts,'zoh');
 
-%%
+%% B
 PM = 60;
 lowBandWidthWcX = 20*2*pi;
 lowBandWidthWcY = 20*2*pi;
@@ -26,6 +29,10 @@ mismatchedBandWidthWcX = 40*2*pi;
 mismatchedBandWidthWcY = 20*2*pi;
 
 LLIlowBandWidthX = getLLI(lowBandWidthWcX,PM,plantX);
+LLIlowBandWidthXD = c2d(LLIlowBandWidthX,Ts,'tustin');
+contlowXPole = pole(feedback(LLIlowBandWidthX*plantX,1))
+discreteLowXPole = pole(feedback(LLIlowBandWidthXD*discretePlantX,1));
+
 LLIlowBandWidthY = getLLI(lowBandWidthWcX,PM,plantY);
 
 LLIHighBandWidthX = getLLI(highBandWidthWcX,PM,plantX);
@@ -49,5 +56,7 @@ bode(LLIHighBandWidthX*plantX/(1+LLIHighBandWidthX*plantX));
 hold off
 title("close loop system of x axis");
 legend("with LBW controller", "with HWB controller");
+
+
 
 
